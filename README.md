e4b9bfb8aaa9e58e143cab16abf2e5ed4728616c
Vivek Parekh <vivekparekh8@gmail.com>
2016-08-22 02:10:31 -0500

    script in jsx

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LearingReact
 Learn React.js from scratch.
+
+## Basics of React
+
+* Transpiling  - Babeljs
+* Components always output only one html element
+* Anything in curly braces {} inside jsx i.e. return (...) will be executed as js
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 export default class Layout extends React.Component{
 	render(){
+		const name = "Vivek";
 		return(
-			<h1>Day 1</h1>
+
+			<h1>Hello All, its {name} here</h1>
 		);
 	}
 }
